d3398155921077e78ed5423199c16d277e03863a
Harrison Cassel <60158937+harrisoncassel@users.noreply.github.com>
2023-04-20 12:48:58 -0400

    add col (state) names to sim_season() output df

--- R/simulations.R
+++ R/simulations.R
@@ -1,12 +1,81 @@
+### Markov Chain simulation functions
+
+simulate_simple_game <- function(trans.mat, n.steps, score=TRUE) {
+  ### Tuned to 20-state game as it worked on 4/10/2023 at 11:20pm
+  # score := TRUE to return data.frame of simulated score; FALSE to
+  #           return boolean indicating if the home team won
+  
+  # Initialize variables
+  home.score <- 0
+  away.score <- 0
+  previous.state <- sample(c(6,16), 1) # initial state
+  # ^ format_events_and_teams() counts a won jumpball as a team inbound, so
+  # This selects one random team to "win" the beginning jumpball and have the
+  # first posession
+  
+  # Simulate game
+  for (i in 1:n.steps) {
+    next.state <- sample(1:20, 1, prob=trans.mat[previous.state, ])
+    
+    if (next.state %in% c(1,2,3)) {
+      home.score <- home.score + next.state # since state # = added score
+    } else if (next.state %in% c(11,12,13)) {
+      away.score <- away.score + (next.state - 10)
+    }
+    
+    previous.state <- next.state
+  }
+  
+  # Return result
+  if (score == TRUE) {
+    result <- data.frame(home=home.score, away=away.score)
+    return(result)
+  } else {
+    result <- TRUE
+    if (home.score < away.score) {result <- FALSE}
+    return(result)
+  }
+}
+
+wl_multi_simple_sim <- function(trans.mat, n.sims=1, n.games=1000, n.steps=300, score=FALSE) {
+  ### Returns list of simulated W/L outcomes for the home team (Team A)
+  # n.steps = 135 is definitely an underestimate (average is soemthing like
+  #           318 transitions per game), but this gets predicted scores to be
+  #           more reasonable (66-63 avg scores, compared to 68-60 2023 avg)
+  #           with only a slight decrease in accuracy (under-estimates home
+  #           advantage by just under 2 % points in the all-2023 test case).
+  results <- c()
+  for (j in 1:n.sims) {
+    total <- 0
+    
+    if (!score) {
+      for (i in 1:n.games) {
+        game.outcome <- simulate_simple_game(trans.mat, n.steps, score=FALSE)
+        total <- total + game.outcome
+      }
+      current <- total / n.games
+      results <- c(results, current)
+    } else {
+      results <- data.frame()
+      for (i in 1:n.games) {
+        new.result <- simulate_simple_game(trans.mat, n.steps, score=TRUE)
+        results <- rbind(results, new.result)
+      }
+    }
+    print(j)
+  }
+  return(results)
+}
+
 multi_sim <- function(trans.mat, n.sims=1, n.games=1000, n.steps=300) {
-  ### Returns a list of dataframes. Each dataframe is a set of outcomes
+  ## Returns a list of dataframes. Each dataframe is a set of outcomes
   # From a different "set" of simulations. Each row is the "stat line"
   # (count of state occurrences) for each simulated game.
   
   previous.state <- sample(c(6,16), 1) # initial state
   # ^ format_events_and_teams() counts a won jumpball as a team inbound, so
   # This selects one random team to "win" the beginning jumpball and have the
-  # first possession
+  # first posession
   
   df.list <- list()
   
@@ -39,18 +108,11 @@ multi_sim <- function(trans.mat, n.sims=1, n.games=1000, n.steps=300) {
 }
 
 sim_season <- function(team.a.id, year, n.steps.per.game=310, n.iter=1000, regular.season.only=TRUE) {
-  ### Returns a data.frame with simulation seasons as rows and stats (chain states) as columns
-  # team.a.id := ESPN team id of interest
-  # year := YYYY year season ends (e.g., 2022-23 season => 2023)
-  # n.steps.per.game := number of state transitions per simulated game
-  # n.iter := number of times to simulate the season
-  # regular.season.only := if TRUE, train and test *only* on regular season games
-  
   team.a.id <- as.integer(team.a.id)
   year <- as.integer(year)
   results.df <- data.frame()
   
-  # Load the PBP data for team of interest (Team A)
+  # Load the PBP data
   pbp <- load_wbb_pbp(year)
   if (regular.season.only) {
     sel.reg.season <- which(pbp$season_type==2)
@@ -58,7 +120,7 @@ sim_season <- function(team.a.id, year, n.steps.per.game=310, n.iter=1000, regul
   }
   sel <- which(pbp$home_team_id == team.a.id | pbp$away_team_id == team.a.id)
   pbp.team <- pbp[sel, ]
-  
+
   # Make Team A's transition matrix
   games <- team_games(team.a.id, pbp=pbp.team)
   team.matrix <- matrix(data=0, nrow=20, ncol=20)
@@ -69,8 +131,9 @@ sim_season <- function(team.a.id, year, n.steps.per.game=310, n.iter=1000, regul
   opp.ids <- c()
   comp.mats <- list()
   game.ids <- unique(pbp.team$game_id)
+  print('teams')
   for (game in 1:length(game.ids)) {
-    # Select game of interest, then non-team A opponent
+    # Select game of interest, then non-team-A opponent
     game.id <- game.ids[game]
     matchup <- pbp[pbp$game_id==game.id, ]
     matchup <- unique(matchup$team_id)
@@ -82,7 +145,7 @@ sim_season <- function(team.a.id, year, n.steps.per.game=310, n.iter=1000, regul
     sel <- which(pbp$home_team_id == opp.id | pbp$away_team_id == opp.id)
     opp.pbp <- pbp[sel, ]
     opp.pbp <- team_games(opp.id, pbp=opp.pbp)
-    
+
     # Create opponent transition matrices
     opp.mat <- matrix(data=0, nrow=20, ncol=20)
     opp.mat <- update_transition_matrix(opp.mat, opp.pbp)
@@ -93,8 +156,9 @@ sim_season <- function(team.a.id, year, n.steps.per.game=310, n.iter=1000, regul
     
     # Add to list
     comp.mats[[game]] <- comp.mat
+    print(game)
   }
-  
+  print('sims')
   for (sim in 1:n.iter) {
     # Run simulation for each season
     stat.line <- matrix(data=0, nrow=1, ncol=21)
@@ -110,19 +174,23 @@ sim_season <- function(team.a.id, year, n.steps.per.game=310, n.iter=1000, regul
     msg <- paste('Completed simulation: ', sim, '/', n.iter, sep='')
     print(msg)
   }
-  
+
+  names(results.df) <- c('A-FT', 'A-2pt', 'A-3pt', 'A-MissShot', 'A-OREB',
+                         'A-TO', 'A-Inbound', 'A-Foul', 'A-DREB', 'A-Block',
+                         'B-FT', 'B-2pt', 'B-3pt', 'B-MissShot', 'B-OREB',
+                         'B-TO', 'B-Inbound', 'B-Foul', 'B-DREB', 'B-Block')
   return(results.df)
 }
 
 sim_season_diagnostic <- function(true.season.box, team.a.id, year, n.steps.per.game=310, n.iter=1000, regular.season.only=TRUE) {
-  ### Returns data.frame with sims as rows and state counts' SQUARED DIFFERENCE from true value as columns 
-  # true.season.box := retrieved using get_true_team_box() from diagnostics.R
+  ### Return df of each var for each run's SQUARE DISTANCE from the true value
+  # true.season.box := retrieved using diagnostics::get_true_team_box()
   # team.a.id := integer value ESPN ID for team of interest
-  # year := year of interest (needs to be same year as that used to get true.season.box -- YYYY format)
+  # year := year of interest (needs to be same year as that of true.season.box, obviously)
   # n.steps.per.game := the number of state transitions per simulation game
   # n.iter := how many times to simulate the season
-  # regular.season.only := if TRUE, train and test *only* on regular season games
-
+  # regular.season.only := TRUE means chains are only trained on reg season data and only reg season games are simulated
+  
   team.a.id <- as.integer(team.a.id)
   year <- as.integer(year)
   results.df <- data.frame()
@@ -192,3 +260,6 @@ sim_season_diagnostic <- function(true.season.box, team.a.id, year, n.steps.per.
   results.df[,'Team'] = team.a.id
   return(results.df)
 }
+
+
+
